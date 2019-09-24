d041a5c66a487a1b5a7782bcda42d25b0d6c6bf1
lmy <1751258687@qq.com>
2019-09-24 10:50:33 +0800

    检查

---
+++ haier-fe/src/views/BranchType.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        分类
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'BranchType',
+        mounted(){
+            axios.get('/0').then(result => {
+
+            }).catch(err => {
+                console.error(err);
+            })
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
---
+++ haier-server/routes/aa.js
@@ -0,0 +1,104 @@
+const aa = {
+    "success": true,
+    "errorCode": null,
+    "message": "从redis获取数据",
+    "data": [
+        {
+            "id": 1183,
+            "idPath": "/",
+            "parentId": 0,
+            "navigationName": "推荐分类",
+            "url": "productCateId=2723;2726;2725;2729;2743;2742;2741",
+            "sortNum": 0,
+            "levels": "2",
+            "imageUrl": null,
+            "linkType": 0,
+            "bannerId": 0
+        },
+        {
+            "id": 1226,
+            "idPath": "/",
+            "parentId": 0,
+            "navigationName": "空洗电器",
+            "url": "productCateId=2725;2729",
+            "sortNum": 1,
+            "levels": "2",
+            "imageUrl": null,
+            "linkType": 0,
+            "bannerId": 0
+        },
+        {
+            "id": 1241,
+            "idPath": "/",
+            "parentId": 0,
+            "navigationName": "冰冷电器",
+            "url": "productCateId=2723;2726",
+            "sortNum": 2,
+            "levels": "2",
+            "imageUrl": null,
+            "linkType": 0,
+            "bannerId": 0
+        },
+        {
+            "id": 931,
+            "idPath": "/",
+            "parentId": 0,
+            "navigationName": "电视影音",
+            "url": "productCateId=2743",
+            "sortNum": 3,
+            "levels": "2",
+            "imageUrl": null,
+            "linkType": 0,
+            "bannerId": 0
+        },
+        {
+            "id": 809,
+            "idPath": "/",
+            "parentId": 0,
+            "navigationName": "厨热电器",
+            "url": "productCateId=2742;2741",
+            "sortNum": 4,
+            "levels": "2",
+            "imageUrl": null,
+            "linkType": 0,
+            "bannerId": 0
+        },
+        {
+            "id": 1681,
+            "idPath": "/",
+            "parentId": 0,
+            "navigationName": "生活小电",
+            "url": "productCateId=4255",
+            "sortNum": 51,
+            "levels": "2",
+            "imageUrl": null,
+            "linkType": 0,
+            "bannerId": 0
+        },
+        {
+            "id": 1836,
+            "idPath": "/",
+            "parentId": 0,
+            "navigationName": "家具家装",
+            "url": "productCateId=3449",
+            "sortNum": 52,
+            "levels": "2",
+            "imageUrl": null,
+            "linkType": 0,
+            "bannerId": 0
+        },
+        {
+            "id": 1629,
+            "idPath": "/",
+            "parentId": 0,
+            "navigationName": "数码3C",
+            "url": "productCateId=2740",
+            "sortNum": 57,
+            "levels": "2",
+            "imageUrl": null,
+            "linkType": 0,
+            "bannerId": 0
+        }
+    ],
+    "totalCount": 0
+}
