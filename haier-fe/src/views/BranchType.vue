47b50c3a7df991de648f3b23a3adff20c96c37d2
qyj <329864251@qq.com>
2019-09-24 10:38:15 +0800

    邱永娇 底部分类路由导航

---
+++ haier-fe/src/views/BranchType.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        分类BranchType
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "BranchType"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ haier-fe/src/views/Login.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        登录login
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Login"
+    }
+</script>
+
+<style scoped>
+
+</style>
